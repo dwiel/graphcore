1fb4e8dc7ba9cc871e58de337666ed6268e9ae7b
Zach Dwiel <zdwiel@plotwatt.com>
2015-10-27 18:16:15 +0000

    move test set up into harness

--- test_graphcore.py
+++ test_graphcore.py
@@ -1,46 +1,7 @@
 import unittest
 
 from . import graphcore
-from . import call_graph
-
-testgraphcore = graphcore.Graphcore()
-
-
-@testgraphcore.rule(['user.name'], 'user.abbreviation')
-def user_abbreviation(name):
-    return ''.join(part[0].upper() for part in name.split(' '))
-
-
-USER_ID_TO_USER_NAME = {
-    1: 'John Smith',
-}
-
-
-@testgraphcore.rule(['user.id'], 'user.name')
-def user_name(id):
-    return USER_ID_TO_USER_NAME[id]
-
-
-testgraphcore.has_many('user', 'books', 'book')
-
-
-@testgraphcore.rule(['user.id'], 'user.books.id', cardinality='many')
-def user_books_id(id):
-    # this would normally come out of a db
-    return [1, 2, 3]
-
-
-BOOK_ID_TO_BOOK_NAME = {
-    1: 'The Giver',
-    2: 'REAMDE',
-    3: 'The Diamond Age',
-}
-
-
-@testgraphcore.rule(['book.id'], 'book.name')
-def book_name(id):
-    return BOOK_ID_TO_BOOK_NAME[id]
-
+from .test_harness import testgraphcore
 
 class hashabledict(dict):
     """ a hashable dict to make it easier to compare query results """
