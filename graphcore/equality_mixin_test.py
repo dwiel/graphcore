0b9ab9c3fdfcd0c78a32bd6c141b6619d3b7b8a5
Zach Dwiel <zdwiel@plotwatt.com>
2015-10-28 21:46:27 +0000

    add tests for EqualityMixin

---
+++ graphcore/equality_mixin_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+from .equality_mixin import EqualityMixin
+
+
+class TestClass(EqualityMixin):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+def test_equality_mixin():
+    x = TestClass(1, 2)
+    y = TestClass(1, 2)
+
+    assert x == y
+
+def test_equality_mixing_other():
+    x = TestClass(1, 2)
+    y = 1
+
+    with pytest.raises(Exception):
+        assert x != y
+
+def test_equality_mixin_with_set():
+    x = TestClass(set([1]), 2)
+    y = TestClass(set([1]), 2)
+
+    assert x == y
