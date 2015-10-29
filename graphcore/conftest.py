70bb96186e7a86ce608db4e818b981641ae1c963
Zach Dwiel <zdwiel@plotwatt.com>
2015-10-29 01:22:22 +0000

    unittest -> pytest

--- graphcore/conftest.py
+++ graphcore/conftest.py
@@ -1,9 +1,16 @@
 import difflib
 
 from .call_graph import CallGraph
-
+from .sql_query import SQLQuery
 
 def call_graph_repr_compare(left, right):
+    return list(difflib.ndiff(
+        repr(left).splitlines(1),
+        repr(right).splitlines(1),
+    ))
+
+
+def sql_query_repr_compare(left, right):
     return list(difflib.ndiff(
         repr(left).splitlines(1),
         repr(right).splitlines(1),
@@ -15,3 +22,6 @@ def pytest_assertrepr_compare(op, left, right):
     if isinstance(left, CallGraph) and isinstance(right, CallGraph):
         if op == '==':
             return call_graph_repr_compare(left, right)
+    elif isinstance(left, SQLQuery) and isinstance(right, SQLQuery):
+        if op == '==':
+            return sql_query_repr_compare(left, right)
--- graphcore/sql_query_test.py
+++ graphcore/sql_query_test.py
@@ -1,58 +1,45 @@
-import unittest
-
 from .sql_query import SQLQuery, mysql_col
 
 
-class AssertSQLQueryEqual():
-    def assertSQLQueryEqual(self, query1, query2):
-        self.assertEqual(query1.tables, query2.tables)
-        self.assertEqual(query1.selects, query2.selects)
-        self.assertEqual(query1.where, query2.where)
-        self.assertEqual(query1.input_mapping, query2.input_mapping)
+def test_simple_query_merge():
+    book_id = SQLQuery(['users', 'books'], 'books.id', {
+        'users.id': 1,
+        'books.user_id': mysql_col('user.id'),
+    })
 
+    combined = SQLQuery(['books'], 'books.name', {
+        'books.id': book_id
+    })
 
-class TestSQLQueryPlan(unittest.TestCase, AssertSQLQueryEqual):
-    def test_simple_query_merge(self):
-        book_id = SQLQuery(['users', 'books'], 'books.id', {
+    assert combined == SQLQuery(
+        'users, books', 'books.name', {
             'users.id': 1,
             'books.user_id': mysql_col('user.id'),
         })
 
-        combined = SQLQuery(['books'], 'books.name', {
-            'books.id': book_id
-        })
+def test_simple_add():
+    first_name = SQLQuery(['users'], 'users.first_name', {
+        'users.id': 1,
+    })
+    last_name = SQLQuery(['users'], 'users.last_name', {
+        'users.id': 1,
+    })
 
-        self.assertSQLQueryEqual(
-            combined,
-            SQLQuery('users, books', 'books.name', {
-                'users.id': 1,
-                'books.user_id': mysql_col('user.id'),
-            })
-        )
-
-    def test_simple_add(self):
-        first_name = SQLQuery(['users'], 'users.first_name', {
+    first_and_last_name = SQLQuery(
+        ['users'], ['users.first_name', 'users.last_name'], {
             'users.id': 1,
-        })
-        last_name = SQLQuery(['users'], 'users.last_name', {
+        }
+    )
+
+    assert first_name + last_name == first_and_last_name
+
+def test_hash():
+    def build():
+        SQLQuery(['users'], 'users.first_name', {
             'users.id': 1,
+        }, {
+            'users.name': 'name',
         })
 
-        first_and_last_name = SQLQuery(
-            ['users'], ['users.first_name', 'users.last_name'], {
-                'users.id': 1,
-            }
-        )
-
-        self.assertSQLQueryEqual(first_name + last_name, first_and_last_name)
-
-    def test_hash(self):
-        def build():
-            SQLQuery(['users'], 'users.first_name', {
-                'users.id': 1,
-            }, {
-                'users.name': 'name',
-            })
-
-        self.assertEqual(hash(build()),  hash(build()))
+    assert hash(build()) == hash(build())
 
