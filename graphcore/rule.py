5e1c9325ca74ea84a65e688377ce147535e731fe
Zach Dwiel <zdwiel@plotwatt.com>
2015-10-29 19:06:43 +0000

    add rule.py

---
+++ graphcore/rule.py
@@ -0,0 +1,38 @@
+import six
+from enum import Enum
+
+from .path import Path
+from .equality_mixin import HashMixin, EqualityMixin
+
+class Cardinality(Enum):
+    one, many = 1, 2
+
+    @staticmethod
+    def cast(init):
+        if isinstance(init, (int, Cardinality)):
+            return Cardinality(init)
+        elif isinstance(init, six.string_types):
+            return Cardinality[init]
+        else:
+            raise TypeError('must be int, str or Cardinality')
+
+
+class Rule(HashMixin, EqualityMixin):
+    def __init__(self, function, inputs, outputs, cardinality):
+        self.function = function
+        self.inputs = sorted([Path(input) for input in inputs])
+        if isinstance(outputs, (Path, six.string_types)):
+            self.outputs = [outputs]
+        else:
+            self.outputs = sorted([Path(output) for output in outputs])
+        self.cardinality = Cardinality.cast(cardinality)
+
+    def __repr__(self):
+        string = '<Rule {outputs} = {function_name}({inputs}) {cardinality}'
+        return string.format(
+                outputs=', '.join(map(str, self.outputs)),
+                function_name=self.function.__name__,
+                inputs=', '.join(map(str, self.inputs)),
+                cardinality=self.cardinality,
+        )
+
