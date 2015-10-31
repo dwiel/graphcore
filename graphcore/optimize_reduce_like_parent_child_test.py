39679eb87b8e1035308196b9cf8f517727ea4118
Zach Dwiel <zdwiel@plotwatt.com>
2015-10-31 01:39:09 +0000

    re #2: ensure parent child optimizer doesnt optimize the wrong type

--- graphcore/optimize_reduce_like_parent_child_test.py
+++ graphcore/optimize_reduce_like_parent_child_test.py
@@ -79,3 +79,39 @@ def test_reduce_like_parent_child_with_two_children():
     assert len(ay.getters) == 0
 
     assert call_graph_expected == call_graph_out
+
+
+def test_reduce_like_parent_child_with_diffent_type():
+    call_graph_in = CallGraph()
+    call_graph_in.add_node(
+        ['a.x'], ['a.y'],
+        Rule(set([1]), ['a.x'], ['a.y'], 'one'),
+    )
+    call_graph_in.add_node(
+        ['a.y'], ['a.z'],
+        Rule(set([2]), ['a.y'], ['a.z'], 'one'),
+    )
+    call_graph_in.edge('a.z').out = True
+
+    call_graph_out = reduce_like_parent_child(
+        call_graph_in, list, list.__add__
+    )
+
+    call_graph_expected = CallGraph()
+    call_graph_expected.add_node(
+        ['a.x'], ['a.y'],
+        Rule(set([1]), ['a.x'], ['a.y'], 'one'),
+    )
+    call_graph_expected.add_node(
+        ['a.y'], ['a.z'],
+        Rule(set([2]), ['a.y'], ['a.z'], 'one'),
+    )
+
+    ay = call_graph_out.edge('a.y')
+    az = call_graph_out.edge('a.z')
+    assert ay.out is False
+    assert az.out
+
+    assert len(ay.getters) == 1
+
+    assert call_graph_expected == call_graph_out
