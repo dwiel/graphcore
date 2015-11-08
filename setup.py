43ec2f231786c7d201dd8a3276980f3a25c9e59d
Zach Dwiel <zdwiel@plotwatt.com>
2015-11-08 03:00:36 +0000

    check python version before install enum34

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+import sys
 
 
 install_requires = []
 install_requires.append('six')
 install_requires.append('sql_query_dict>=0.5')
-install_requires.append('enum34')
+if sys.version_info < (3, 4):
+    install_requires.append('enum34')
 
 
 setup(
