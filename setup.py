941eaa7d0e2f52646911d5cdfe370238aba0f460
Zach Dwiel <zdwiel@plotwatt.com>
2015-11-08 02:56:52 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+install_requires = []
+install_requires.append('six')
+install_requires.append('sql_query_dict>=0.5')
+install_requires.append('enum34')
+
+
+setup(
+    name='graphcore',
+    version='0.1',
+    description='Graphcore is a python library which allows you to query a '
+                'graph structure with a query language similar to MQL, '
+                'Falcor or GraphQL',
+    url='http://github.com/dwiel/graphcore',
+    author='Zach Dwiel',
+    author_email='zdwiel@gmail.com',
+    license='Apache',
+    py_modules=['graphcore'],
+    install_requires=install_requires,
+)
