4370e03705953ab97db957d1fa39f9b4b00556c9
Zach Dwiel <zdwiel@plotwatt.com>
2015-11-09 02:45:32 +0000

    version 0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 4):
 
 setup(
     name='graphcore',
-    version='0.3',
+    version='0.4',
     description='Graphcore is a python library which allows you to query a '
                 'graph structure with a query language similar to MQL, '
                 'Falcor or GraphQL',
