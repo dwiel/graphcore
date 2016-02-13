cf827d9c1bc142f84b5675d0186613c9d83f506b
Zach Dwiel <zdwiel@plotwatt.com>
2016-02-13 15:09:00 +0000

    pep8

--- graphcore/query_test.py
+++ graphcore/query_test.py
@@ -1,7 +1,8 @@
 from .query import Query
 
+
 def test_contains():
     # ensure that parsing a query with a contains operator works
     Query({
-        'x.id|=': [1,2,3],
+        'x.id|=': [1, 2, 3],
     })
